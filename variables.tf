ec383d6bba7d7c89ca0861b6636098dda08bbfcf
yuhkih <yuhkih@hotmail.com>
2025-04-30 19:36:25 +0900

    add comment

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "rosa_openshift_version" {
   type        = string
-  default     = "4.15.0"
+  default     = "4.15.29"
   description = "Desired version of OpenShift for the cluster, for example '4.15.0'. If version is greater than the currently running version, an upgrade will be scheduled."
 }
 
@@ -57,7 +57,7 @@ variable "machine_type" {
 
 variable "worker_node_replicas" {
   default     = 2
-  description = "Number of worker nodes to provision. Single zone clusters need at least 2 nodes, multizone clusters need at least 3 nodes"
+  description = "Number of worker nodes to provision. Single zone clusters need at least 2 nodes, multizone clusters need at least 3 nodes. For RHOAM, you need at least 5 worker nodes"
   type        = number
 }
 
